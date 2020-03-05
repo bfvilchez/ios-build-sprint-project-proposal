ef45e9b33b391c16476c1af6ab3bace66b1eb2a6
brian <bfvilchez95@gmail.com>
2020-03-05 11:32:43 -0700

    updated fetchFromServer method

---
+++ Pixels/Pixels/Model/PictureRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  PictureRepresentation.swift
+//  Pixels
+//
+//  Created by brian vilchez on 3/4/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import Foundation
---
+++ Pixels/Pixels/Resources/http+secured.swift
@@ -0,0 +1,9 @@
+//
+//  http+secured.swift
+//  Pixels
+//
+//  Created by brian vilchez on 3/5/20.
+//  Copyright © 2020 brian vilchez. All rights reserved.
+//
+
+import Foundation
